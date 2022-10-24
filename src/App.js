80941554ea95643cf84ae05c3c6dd4c2e8a7df42
bcause-sys01 <sys01@b-cause.co.jp>
2022-10-24 22:05:23 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState("Data Area");
+  const onCLick = async () => {
+    try {
+      const res = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts',
+      );
+      setData(res.data)
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header-section'>
+        <div className='header-text'>This is my react app</div>
       </header>
+      <div className='button' onClick={onCLick}>click</div>
+      <h4>data area</h4>
+      {data && 
+        <div>{JSON.stringify(data[0].title,null)}</div>
+      }
+      
     </div>
   );
 }
